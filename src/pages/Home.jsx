d531a6229ba4eefe47e36f78bf89c0cb111bb1c2
Teri Chudějová <tereza.chudejova@gmail.com>
2024-09-10 10:21:26 +0200

    add responsivity to smaller widths

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,7 +2,7 @@ export default function Home() {
     return (
         <div className="outerBox">
             <div className="innerBox">  
-                <h2>Home</h2>
+                <h1>Home</h1>
                 <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Magni doloribus nulla dignissimos praesentium tenetur porro? Ducimus totam voluptas ea error, laboriosam dolores ab inventore quis aliquam nisi mollitia nulla! Inventore?</p>
             </div>
         </div>
