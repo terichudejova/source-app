bf1b5b571191dc3fd5f3e0d06bb56cf39204b350
Teri Chudějová <tereza.chudejova@gmail.com>
2024-09-10 17:30:07 +0200

    integrate SASS

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { useState, useEffect } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css"
 import { Outlet, NavLink } from 'react-router-dom'
 import ScrollToTop from './ScrollToTop';
 
--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import './index.css'
+import "./index.css"
 import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 import Home from "./pages/Home.jsx";
 import About from "./pages/About.jsx";
